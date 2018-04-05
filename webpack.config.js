04ebc3142ea5392e0216dd2df17e3aca2ac52333
Andy <mindovermiles262@gmail.com>
2018-04-05 11:49:52 -0700

    initial

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8080,
+    compress: true
+  }
+
+};
+
